abb0b78cd5045eee1f38cfd5dc33d75167f64fca
Abhishek <abhisekgaire7@gmail.com>
2024-02-14 15:48:26 +0545

    MVC

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error")
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views" , "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop")
 
 
@@ -17,11 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //Middleware
 app.use(express.static(path.join(__dirname, "public"))); //Static files
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render("404", {path: "",pageTitle: "Page Not Found"});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
